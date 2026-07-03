05cf2eba1165371c2ff70738a5817f6d59f9fa6a
caroWD <juego.fuego@gmail.com>
2026-07-03 16:33:48 -0500

    feat: configure dark mode theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button'
+import { ModeToggle } from '@/components/mode-toggle'
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <ModeToggle />
     </div>
   )
 }
