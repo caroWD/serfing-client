c97abb935962042054baf6ea4f3545de3a1162b4
caroWD <juego.fuego@gmail.com>
2026-07-03 16:13:15 -0500

    feat: configure shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import { resolve } from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
